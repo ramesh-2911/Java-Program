2d9b56a1a0f641ae4f382a40c2c6399d3895b33e
ramesh-2911 <rameshruthik130@gmail.com>
2024-11-14 15:01:08 +0530

    Add files via upload

---
+++ exp1/LinearSearch.java
@@ -0,0 +1,14 @@
+public class LinearSearch 
+{
+public static void main(String[] args) {
+int[] arr = {2,4,5,6,12,18,22,31,49};
+int key = 28;
+int result = linearSearch(arr, key);
+{
+System.out.println("Element found at index: ");
+}
+else {
+System.out.println("Element not found in the array");
+}
+}
+}
